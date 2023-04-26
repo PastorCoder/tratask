18bf14121fb1ee041d9d051b0399db47798665fa
pastorcoder <zuma4good@gmail.com>
2023-04-26 08:56:57 +0100

    added connect to Test page

--- src/component/pages/Test.js
+++ src/component/pages/Test.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Connect from "../frames/Connect";
 
 function Test() {
   return (
     <div>
           <h1>This is another test</h1>
-          <h3>I trust the Lord Jesus Christ</h3>
+      <h3>I trust the Lord Jesus Christ</h3>
+      <Connect />
     </div>
   );
 }
