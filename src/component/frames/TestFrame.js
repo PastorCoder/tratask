6d212172dec6270f5768517981049694da3b73f3
pastorcoder <zuma4good@gmail.com>
2023-04-27 00:05:09 +0100

    testing and redoing again again

--- src/component/frames/TestFrame.js
+++ src/component/frames/TestFrame.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Transfer from "../frames/Transfer";
+import TestConnect from "../frames/TestConnect";
 import { RecoilRoot } from "recoil";
 
 function Test() {
   return (
     <div>
-        <Transfer />
+      <TestConnect />
     </div>
   );
 }
