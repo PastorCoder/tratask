a950531a28902838f796264f47d386d62c300644
pastorcoder <zuma4good@gmail.com>
2023-04-26 09:11:53 +0100

    added testFrame

--- src/component/frames/TestFrame.js
+++ src/component/frames/TestFrame.js
@@ -1,12 +1,10 @@
 import React from "react";
-import TestFrame from "../frames/TestFrame";
 
 function Test() {
   return (
     <div>
       <h1>This is another test</h1>
       <h3>I trust the Lord Jesus Christ</h3>
-      <Test />
     </div>
   );
 }
