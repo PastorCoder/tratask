33b060ae75462bbd8e72a9d29317ac61839fb1e4
pastorcoder <zuma4good@gmail.com>
2023-04-26 09:03:20 +0100

    removed Connect page and added Transfer to Test page

--- src/component/pages/Test.js
+++ src/component/pages/Test.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Connect from "../frames/Connect";
+import Transfer from "../frames/Transfer";
 
 function Test() {
   return (
     <div>
-          <h1>This is another test</h1>
+      <h1>This is another test</h1>
       <h3>I trust the Lord Jesus Christ</h3>
-      <Connect />
+      <Transfer />
     </div>
   );
 }
