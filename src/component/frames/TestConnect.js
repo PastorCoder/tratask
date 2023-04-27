a800bc77a58d89875395e8b3c9b2e74b2a96c69d
pastorcoder <zuma4good@gmail.com>
2023-04-27 03:02:25 +0100

    WILL IT WORK again again

--- src/component/frames/TestConnect.js
+++ src/component/frames/TestConnect.js
@@ -108,16 +108,7 @@ function TestConnect() {
 
     
 
-  useEffect(() => {
-     const setup = async () => {
-       // Construct
-       const wsProvider = new WsProvider("wss://rpc.polkadot.io");
-       const api = await ApiPromise.create({ provider: wsProvider });
-
-       // Do something
-       console.log(api.genesisHash.toHex());
-     };
-  }, []);
+  
 
 
     
